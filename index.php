dde21eeb69ef4333fe573336e60ae7d8b609ec21
Imad gannoun <75321420+ImadBen96@users.noreply.github.com>
2023-07-18 23:13:55 +0200

    add loader class to load controllers

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "hii ismail !!!";
+require __DIR__ . '/vendor/System/Application.php';
+require __DIR__ . '/vendor/System/File.php';
+
+use System\File;
+use System\Application;
+
+$file = new File(__DIR__);
+
+$app = Application::getInstance($file);
+
+
+
